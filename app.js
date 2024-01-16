22fa4746a2a4d36c55fbe0ffea690dbf4eee3286
Lee Powell <81479565+leepowe@users.noreply.github.com>
2024-01-15 22:15:30 -0500

    for some reason .recognize isn't being recognized as a function, research time

--- app.js
+++ app.js
@@ -7,21 +7,40 @@ const worker = createWorker();
 
 //Storage
 const storage = multer.diskStorage({
-    destination: (req, res, cb) => {
-        cb(null, './uploads');
-    },
-    filename: (req, res, cb) => {
-        cb(null, req.file);
-    }
+	destination: (req, file, cb) => {
+		cb(null, './uploads');
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname);
+	},
 });
 const upload = multer({ storage: storage }).single('avatar');
 
 app.set('view engine', 'ejs');
 
-// app.get('/uploads', (req, res) => {
-//     console.log('uploads');
-// })
+//Routes
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+	upload(req, res, (err) => {
+		fs.readFile(`./uploads/${req.file.originalname}`, (err, data) => {
+			if (err) return console.log('This is your error', err);
+
+			worker
+				.recognize(data, "eng", { tessjs_create_pdf: "1" })
+				.progress((progress) => {
+					console.log(progress);
+				})
+				.then(result => {
+					res.send(result.text);
+				})
+				.finally(() => worker.terminate());
+		});
+	});
+});
 
 //Start up server
 const PORT = 5000 || process.env.PORT;
-app.listen(PORT, () => console.log("Hey I'm running on port ${PORT}"));
+app.listen(PORT, () => console.log(`Hey I'm running on port ${PORT}`));
